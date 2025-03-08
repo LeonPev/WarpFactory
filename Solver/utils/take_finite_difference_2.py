c2deba5f27343ddc8531782cd90183ca08df70ee
test <test@example.com>
2025-03-08 23:25:14 +0000

    feat: complete conversion of takeFiniteDifference2.m

--- Solver/utils/take_finite_difference_2.py
+++ Solver/utils/take_finite_difference_2.py
@@ -8,15 +8,15 @@ def take_finite_difference_2(A, k1, k2, delta, phiphi_flag=False):
     if s[k1] >= 5 and s[k2] >= 5:
         if k1 == k2:
             if k1 == 0:
-                B[2:-2] = (-(A[4:] + A[:-4]) + 16 * (A[3:-1] + A[1:-3]) - 30 * A[2:-2]) / (12 * delta**2)
+                B[2:-2] = (-(A[4:] + A[:-4]) + 16 * (A[3:-1] + A[1:-3]) - 30 * A[2:-2]) / (12 * delta[k1]**2)
                 B[:2] = B[2]
                 B[-2:] = B[-3]
             elif k1 == 1:
-                B[:, 2:-2] = (-(A[:, 4:] + A[:, :-4]) + 16 * (A[:, 3:-1] + A[:, 1:-3]) - 30 * A[:, 2:-2]) / (12 * delta**2)
+                B[:, 2:-2] = (-(A[:, 4:] + A[:, :-4]) + 16 * (A[:, 3:-1] + A[:, 1:-3]) - 30 * A[:, 2:-2]) / (12 * delta[k1]**2)
                 B[:, :2] = B[:, 2]
                 B[:, -2:] = B[:, -3]
             elif k1 == 2:
-                B[:, :, 2:-2] = (-(A[:, :, 4:] + A[:, :, :-4]) + 16 * (A[:, :, 3:-1] + A[:, :, 1:-3]) - 30 * A[:, :, 2:-2]) / (12 * delta**2)
+                B[:, :, 2:-2] = (-(A[:, :, 4:] + A[:, :, :-4]) + 16 * (A[:, :, 3:-1] + A[:, :, 1:-3]) - 30 * A[:, :, 2:-2]) / (12 * delta[k1]**2)
                 if phiphi_flag:
                     B[:, :, :2] = -2
                     B[:, :, -2:] = 2
@@ -24,71 +24,25 @@ def take_finite_difference_2(A, k1, k2, delta, phiphi_flag=False):
                     B[:, :, :2] = B[:, :, 2]
                     B[:, :, -2:] = B[:, :, -3]
             elif k1 == 3:
-                B[:, :, :, 2:-2] = (-(A[:, :, :, 4:] + A[:, :, :, :-4]) + 16 * (A[:, :, :, 3:-1] + A[:, :, :, 1:-3]) - 30 * A[:, :, :, 2:-2]) / (12 * delta**2)
+                B[:, :, :, 2:-2] = (-(A[:, :, :, 4:] + A[:, :, :, :-4]) + 16 * (A[:, :, :, 3:-1] + A[:, :, :, 1:-3]) - 30 * A[:, :, :, 2:-2]) / (12 * delta[k1]**2)
                 B[:, :, :, :2] = B[:, :, :, 2]
                 B[:, :, :, -2:] = B[:, :, :, -3]
         else:
             kL = max(k1, k2)
             kS = min(k1, k2)
 
-            x2 = slice(4, None)
-            x1 = slice(3, -1)
-            x0 = slice(2, -2)
-            x_1 = slice(1, -3)
-            x_2 = slice(0, -4)
-
-            y2 = slice(4, None)
-            y1 = slice(3, -1)
-            y0 = slice(2, -2)
-            y_1 = slice(1, -3)
-            y_2 = slice(0, -4)
-            
             sl = [slice(None)] * 4
+            sl[kS] = slice(2, -2)
+            sl[kL] = slice(2, -2)
 
-            if kS == 0:
-                sl[kS] = x0
-                if kL == 1:
-                    sl[kL] = y0
-                    B[tuple(sl)] = 1 / (12**2 * delta**2) * (
-                        -(-(A[tuple([x2 if i == kS else y2 for i in range(4)])]  - A[tuple([x_2 if i == kS else y2 for i in range(4)])] ) + 8 * (A[tuple([x1 if i == kS else y2 for i in range(4)])]  - A[tuple([x_1 if i == kS else y2 for i in range(4)])])) +
-                        (-(A[tuple([x2 if i == kS else y_2 for i in range(4)])] - A[tuple([x_2 if i == kS else y_2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_2 for i in range(4)])] - A[tuple([x_1 if i == kS else y_2 for i in range(4)])])) +
-                        8 * (-(A[tuple([x2 if i == kS else y1 for i in range(4)])]  - A[tuple([x_2 if i == kS else y1 for i in range(4)])] ) + 8 * (A[tuple([x1 if i == kS else y1 for i in range(4)])]  - A[tuple([x_1 if i == kS else y1 for i in range(4)])])) -
-                        8 * (-(A[tuple([x2 if i == kS else y_1 for i in range(4)])] - A[tuple([x_2 if i == kS else y_1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_1 for i in range(4)])] - A[tuple([x_1 if i == kS else y_1 for i in range(4)])]))
-                    )
-                elif kL == 2:
-                    sl[kL] = y0
-                    B[tuple(sl)] = 1 / (12**2 * delta[kS] * delta[kL]) * (
-                        -(-(A[tuple([x2 if i == kS else y2 for i in range(4)])] - A[tuple([x_2 if i == kS else y2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y2 for i in range(4)])] - A[tuple([x_1 if i == kS else y2 for i in range(4)])])) +
-                        (-(A[tuple([x2 if i == kS else y_2 for i in range(4)])] - A[tuple([x_2 if i == kS else y_2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_2 for i in range(4)])] - A[tuple([x_1 if i == kS else y_2 for i in range(4)])])) +
-                        8 * (-(A[tuple([x2 if i == kS else y1 for i in range(4)])] - A[tuple([x_2 if i == kS else y1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y1 for i in range(4)])] - A[tuple([x_1 if i == kS else y1 for i in range(4)])])) -
-                        8 * (-(A[tuple([x2 if i == kS else y_1 for i in range(4)])] - A[tuple([x_2 if i == kS else y_1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_1 for i in range(4)])] - A[tuple([x_1 if i == kS else y_1 for i in range(4)])]))
-                    )
-                elif kL == 3:
-                    sl[kL] = y0
-                    B[tuple(sl)] = 1 / (12**2 * delta[kS] * delta[kL]) * (
-                        -(-(A[tuple([x2 if i == kS else y2 for i in range(4)])] - A[tuple([x_2 if i == kS else y2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y2 for i in range(4)])] - A[tuple([x_1 if i == kS else y2 for i in range(4)])])) +
-                        (-(A[tuple([x2 if i == kS else y_2 for i in range(4)])] - A[tuple([x_2 if i == kS else y_2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_2 for i in range(4)])] - A[tuple([x_1 if i == kS else y_2 for i in range(4)])])) +
-                        8 * (-(A[tuple([x2 if i == kS else y1 for i in range(4)])] - A[tuple([x_2 if i == kS else y1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y1 for i in range(4)])] - A[tuple([x_1 if i == kS else y1 for i in range(4)])])) -
-                        8 * (-(A[tuple([x2 if i == kS else y_1 for i in range(4)])] - A[tuple([x_2 if i == kS else y_1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_1 for i in range(4)])] - A[tuple([x_1 if i == kS else y_1 for i in range(4)])]))
-                    )
-                # Other cases for kS and kL
-            elif kS == 1:
-                sl[kS] = x0
-                if kL == 2:
-                    sl[kL] = y0
-                    B[tuple(sl)] = 1 / (12**2 * delta**2) * (
-                        -(-(A[tuple([x2 if i == kS else y2 for i in range(4)])] - A[tuple([x_2 if i == kS else y2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y2 for i in range(4)])] - A[tuple([x_1 if i == kS else y2 for i in range(4)])])) +
-                        (-(A[tuple([x2 if i == kS else y_2 for i in range(4)])] - A[tuple([x_2 if i == kS else y_2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_2 for i in range(4)])] - A[tuple([x_1 if i == kS else y_2 for i in range(4)])])) +
-                        8 * (-(A[tuple([x2 if i == kS else y1 for i in range(4)])] - A[tuple([x_2 if i == kS else y1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y1 for i in range(4)])] - A[tuple([x_1 if i == kS else y1 for i in range(4)])])) -
-                        8 * (-(A[tuple([x2 if i == kS else y_1 for i in range(4)])] - A[tuple([x_2 if i == kS else y_1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_1 for i in range(4)])] - A[tuple([x_1 if i == kS else y_1 for i in range(4)])]))
-                    )
-                elif kL == 3:
-                    sl[kL] = y0
-                    B[tuple(sl)] = 1 / (12**2 * delta**2) * (
-                        -(-(A[tuple([x2 if i == kS else y2 for i in range(4)])] - A[tuple([x_2 if i == kS else y2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y2 for i in range(4)])] - A[tuple([x_1 if i == kS else y2 for i in range(4)])])) +
-                        (-(A[tuple([x2 if i == kS else y_2 for i in range(4)])] - A[tuple([x_2 if i == kS else y_2 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_2 for i in range(4)])] - A[tuple([x_1 if i == kS else y_2 for i in range(4)])])) +
-                        8 * (-(A[tuple([x2 if i == kS else y1 for i in range(4)])] - A[tuple([x_2 if i == kS else y1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y1 for i in range(4)])] - A[tuple([x_1 if i == kS else y1 for i in range(4)])])) -
-                        8 * (-(A[tuple([x2 if i == kS else y_1 for i in range(4)])] - A[tuple([x_2 if i == kS else y_1 for i in range(4)])]) + 8 * (A[tuple([x1 if i == kS else y_1 for i in range(4)])] - A[tuple([x_1 if i == kS else y_1 for i in range(4)])]))
-                    )
+            def f(i, kS, kL, x, y):
+                return [x if j == kS else y for j in range(4)]
+
+            B[tuple(sl)] = 1 / (12**2 * delta[kS] * delta[kL]) * (
+                -(-(A[tuple(f(0, kS, kL, 4, 4))] - A[tuple(f(1, kS, kL, 0, 4))]) + 8 * (A[tuple(f(2, kS, kL, 3, 4))] - A[tuple(f(3, kS, kL, 1, 4))])) +
+                (-(A[tuple(f(4, kS, kL, 4, 0))] - A[tuple(f(5, kS, kL, 0, 0))]) + 8 * (A[tuple(f(6, kS, kL, 3, 0))] - A[tuple(f(7, kS, kL, 1, 0))])) +
+                8 * (-(A[tuple(f(8, kS, kL, 4, 3))] - A[tuple(f(9, kS, kL, 0, 3))]) + 8 * (A[tuple(f(10, kS, kL, 3, 3))] - A[tuple(f(11, kS, kL, 1, 3))])) -
+                8 * (-(A[tuple(f(12, kS, kL, 4, 1))] - A[tuple(f(13, kS, kL, 0, 1))]) + 8 * (A[tuple(f(14, kS, kL, 3, 1))] - A[tuple(f(15, kS, kL, 1, 1))]))
+            )
 
     return B
